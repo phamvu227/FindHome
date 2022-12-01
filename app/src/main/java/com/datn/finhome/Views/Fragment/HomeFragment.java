41d26e968b8e7f308d423005e7b612aa2ac9c3b8
Văn Thức <72640345+thucvan@users.noreply.github.com>
2022-12-01 12:52:21 +0700

    thucvan - 1/12/2022 upload data main

--- app/src/main/java/com/datn/finhome/Views/Fragment/HomeFragment.java
+++ app/src/main/java/com/datn/finhome/Views/Fragment/HomeFragment.java
@@ -2,16 +2,33 @@ package com.datn.finhome.Views.Fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.datn.finhome.Adapter.RoomAdapter;
+import com.datn.finhome.Models.RoomModel;
 import com.datn.finhome.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+    private RoomAdapter roomAdapter;
+    private List<RoomModel> mRoomModel;
+    private RecyclerView rcv;
+    private DatabaseReference reference;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +38,6 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         return view;
     }
@@ -30,14 +46,28 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @androidx.annotation.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initView(view);
+        reference = FirebaseDatabase.getInstance().getReference("Room");
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mRoomModel.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    RoomModel roomModel = dataSnapshot.getValue(RoomModel.class);
+                    mRoomModel.add(roomModel);
+                    roomAdapter = new RoomAdapter(getContext(), mRoomModel);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("TAG", error.getMessage());
+            }
+        });
+        rcv.setAdapter(roomAdapter);
     }
 
-    private void initView(View view) {}
-
-//    private void setUpSearch() {
-//        btnSearch.setOnClickListener(v -> {
-//            Intent intent = new Intent(getContext(), SearchActivity.class);
-//            startActivity(intent);
-//        });
-//    }
+    private void initView(View view) {
+        mRoomModel = new ArrayList<>();
+        rcv = view.findViewById(R.id.rcvRoomMain);
+    }
 }
