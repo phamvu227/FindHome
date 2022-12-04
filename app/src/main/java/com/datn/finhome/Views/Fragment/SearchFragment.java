aec02e2a11b2423c1bd20fc8dd296ffb4a09aa09
GIGABYTE <vuplph17965@fpt.edu.vn>
2022-12-04 15:43:20 +0700

    search

--- app/src/main/java/com/datn/finhome/Views/Fragment/SearchFragment.java
+++ app/src/main/java/com/datn/finhome/Views/Fragment/SearchFragment.java
@@ -2,16 +2,37 @@ package com.datn.finhome.Views.Fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.datn.finhome.Adapter.RoomAdapter;
+import com.datn.finhome.Models.RoomModel;
 import com.datn.finhome.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchFragment extends Fragment {
+    private SearchView searchView;
+    private RecyclerView recyclerSearch;
+    private List<RoomModel> list;
+    private DatabaseReference dbReference;
+
+    private RoomAdapter roomAdapter;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +43,7 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.activity_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
         return view;
     }
 
@@ -33,6 +54,33 @@ public class SearchFragment extends Fragment {
     }
 
     private void initView(View view) {
+        searchView = view.findViewById(R.id.txt_search);
+
+        recyclerSearch = view.findViewById(R.id.recycle_list_search);
+
+        dbReference = FirebaseDatabase.getInstance().getReference("Room");
+    }
+
+    private void roomSearch() {
+        list = new ArrayList<>();
+        dbReference = FirebaseDatabase.getInstance().getReference("Room");
+        dbReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    RoomModel roomModel = dataSnapshot.getValue(RoomModel.class);
+                    list.add(roomModel);
+                    roomAdapter = new RoomAdapter(getContext(), list);
+                    recyclerSearch.setAdapter(roomAdapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("TAG", error.getMessage());
+            }
+        });
 
     }
 }
