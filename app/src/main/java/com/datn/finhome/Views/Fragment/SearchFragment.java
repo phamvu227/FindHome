3243ba06a78e8f419c9ae04d40fd20497e1e04c5
Văn Thức <72640345+thucvan@users.noreply.github.com>
2022-12-07 16:10:22 +0700

    thucvan - 7/12/2022 add rcv SearchFragment

--- app/src/main/java/com/datn/finhome/Views/Fragment/SearchFragment.java
+++ app/src/main/java/com/datn/finhome/Views/Fragment/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.datn.finhome.Views.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -18,6 +19,8 @@ import com.datn.finhome.Adapter.RoomAdapter;
 import com.datn.finhome.IClickItemUserListener;
 import com.datn.finhome.Models.RoomModel;
 import com.datn.finhome.R;
+import com.datn.finhome.Utils.LoaderDialog;
+import com.datn.finhome.Views.Activity.ShowDetailActivity;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -33,7 +36,6 @@ public class SearchFragment extends Fragment implements IClickItemUserListener {
     private RecyclerView recyclerSearch;
     private List<RoomModel> list;
     private DatabaseReference dbReference;
-
     private RoomAdapter roomAdapter;
 
     @Override
@@ -45,8 +47,8 @@ public class SearchFragment extends Fragment implements IClickItemUserListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+        LoaderDialog.createDialog(requireActivity());
         return view;
     }
 
@@ -54,15 +56,31 @@ public class SearchFragment extends Fragment implements IClickItemUserListener {
     public void onViewCreated(@NonNull View view, @androidx.annotation.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initView(view);
+        list = new ArrayList<>();
+        dbReference = FirebaseDatabase.getInstance().getReference("Room");
+        dbReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    RoomModel roomModel = dataSnapshot.getValue(RoomModel.class);
+                    list.add(roomModel);
+                    roomAdapter = new RoomAdapter(getContext(), list, roomModel1 -> onClickGoToDetail(roomModel1));
+                    recyclerSearch.setAdapter(roomAdapter);
+                }
+                LoaderDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("TAG", error.getMessage());
+            }
+        });
     }
 
     private void initView(View view) {
-        dbReference = FirebaseDatabase.getInstance().getReference().child("Room");
         searchView = view.findViewById(R.id.txt_search);
-
         recyclerSearch = view.findViewById(R.id.recycle_list_search);
-
-
     }
 
     @Override
@@ -116,11 +134,16 @@ public class SearchFragment extends Fragment implements IClickItemUserListener {
         }
         RoomAdapter roomAdapter = new RoomAdapter(getContext(), myList, this);
         recyclerSearch.setAdapter(roomAdapter);
-
     }
 
     @Override
-    public void onClickItemRoom(RoomModel roomModel) {
+    public void onClickItemRoom(RoomModel roomModel) {}
 
+    private  void onClickGoToDetail(RoomModel roomModel){
+        Intent intent = new Intent(getActivity(), ShowDetailActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("Room", roomModel);
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 }
