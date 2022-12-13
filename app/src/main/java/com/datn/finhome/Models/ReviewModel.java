37605eb1613bc937ee6874e7e676dda4699edb46
minhqd <minhqd2808@gmail.com>
2022-12-13 19:40:54 +0700

    code phan binh luan

---
+++ app/src/main/java/com/datn/finhome/Models/ReviewModel.java
@@ -0,0 +1,20 @@
+package com.datn.finhome.Models;
+
+public class ReviewModel {
+    String reviews;
+
+    public ReviewModel(String reviews) {
+        this.reviews = reviews;
+    }
+
+    public ReviewModel() {
+    }
+
+    public String getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(String reviews) {
+        this.reviews = reviews;
+    }
+}
--- app/src/main/java/com/datn/finhome/Views/Activity/ShowDetailActivity.java
+++ app/src/main/java/com/datn/finhome/Views/Activity/ShowDetailActivity.java
@@ -1,8 +1,18 @@
 package com.datn.finhome.Views.Activity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,8 +23,10 @@ import androidx.appcompat.widget.AppCompatImageButton;
 
 import com.bumptech.glide.Glide;
 import com.datn.finhome.Models.HostModel;
+import com.datn.finhome.Models.ReviewModel;
 import com.datn.finhome.Models.RoomModel;
 import com.datn.finhome.R;
+import com.datn.finhome.Utils.LoaderDialog;
 import com.datn.finhome.databinding.ActivityHostDetailsBinding;
 import com.datn.finhome.databinding.ActivityShowDetailsBinding;
 import com.google.android.material.button.MaterialButton;
@@ -87,5 +99,59 @@ public class ShowDetailActivity extends AppCompatActivity {
                 Log.e("TAG", "onCancelled: " + error.getMessage());
             }
         });
+    }
+
+    private  void openDialog(){
+//        final Dialog dialog = new Dialog(this);
+//        dialog.setContentView(R.layout.review_dialog);
+//        dialog.setTitle("AA");
+//        dialog.show();
+        final Dialog dialog = new Dialog(this);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setContentView(R.layout.review_dialog);
+
+        Window window = dialog.getWindow();
+        if(window == null){
+            return;
+        }
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        //window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windowatribute = window.getAttributes();
+        windowatribute.gravity = Gravity.CENTER;
+        window.setAttributes(windowatribute);
+
+        EditText edtReviews = dialog.findViewById(R.id.edtReviews);
+        Button btnCancel = dialog.findViewById(R.id.btnCancel);
+        Button btnSend = dialog.findViewById(R.id.btnSend);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+                ReviewModel reviewModel = new ReviewModel(
+                        edtReviews.getText().toString().trim()
+                );
+                mDatabase.child("Reviews").push().setValue(reviewModel, (databaseError, databaseReference) -> {
+                    if (databaseError != null) {
+                        Toast.makeText(ShowDetailActivity.this, "Lỗi: " + databaseError + "", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(ShowDetailActivity.this, "Đã gửi bình luận", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+    public void reviews(View view) {
+        openDialog();
     }
 }
