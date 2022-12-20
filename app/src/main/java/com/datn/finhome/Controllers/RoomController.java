04a74f6d6dce435c6b367051685ac913811429f7
LeDucBao1637 <97378626+baold02@users.noreply.github.com>
2022-12-20 11:15:53 +0700

    Fix chat Done hostDetail, fix add room

--- app/src/main/java/com/datn/finhome/Controllers/RoomController.java
+++ app/src/main/java/com/datn/finhome/Controllers/RoomController.java
@@ -18,7 +18,7 @@ public class RoomController {
     }
 
     public void insertProduct(RoomModel room, IAfterInsertObject iAfterInsertObject) {
-        FirebaseDatabase.getInstance().getReference().child("Room")
+        FirebaseDatabase.getInstance().getReference().child("Room").child(room.getId())
                 .setValue(room, (error, ref) -> {
                     if (error == null) {
                         iAfterInsertObject.onSuccess(room);
--- app/src/main/java/com/datn/finhome/Views/Activity/FavoriteActivity.java
+++ app/src/main/java/com/datn/finhome/Views/Activity/FavoriteActivity.java
@@ -6,23 +6,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
-
 import com.datn.finhome.Adapter.AdapterFavorite;
-import com.datn.finhome.Models.ReviewModel;
 import com.datn.finhome.Models.RoomModel;
-import com.datn.finhome.R;
 import com.datn.finhome.databinding.ActivityFavoriteBinding;
-import com.datn.finhome.databinding.ActivityShowDetailsBinding;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import org.checkerframework.checker.units.qual.A;
 
 import java.util.ArrayList;
 
